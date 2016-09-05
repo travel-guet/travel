9b90bcc1a6b0731957f0e1898a7de445d8944cea
yuewe <2523430760@qq.com>
2016-09-05 18:41:00 +0800

    演示

--- app/src/main/java/com/example/guet/travel/MainActivity.java
+++ app/src/main/java/com/example/guet/travel/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 		System.out.println("helloworld");
 		System.out.println("my name is yue");
+		System.out.println("yuewe");
     }
 }
